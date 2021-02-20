fb1dda199ef930d871659e9867d1133f792de854
Kid <Kid.huang@cygia.com>
2021-02-20 18:33:21 +0800

    1.修改协议文档 2.在排队之前会先查询餐厅状态

--- 小程序/app.js
+++ 小程序/app.js
@@ -17,6 +17,11 @@ App({
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        console.log('code ='+res.code); // 先login得到code
+        if (res.code) {
+        // 将url中的appid和secret换成自己的  （可以在开发平台查看）
+          this.globalData.openCode = res.code
+        }
       }
     })
     // 获取用户信息
@@ -40,6 +45,7 @@ App({
       }
     })
     //获取服务器餐厅数据
+    console.log('Try to connect the server')
     this.getMerchantInfo()
   },
   getMerchantInfo: function () {
@@ -47,21 +53,36 @@ App({
       url: this.globalData.host + '/searchMerchant/',
       method: 'GET',
       success: function (data) {
-        console.log('data = '+data)
+        console.log('data = '+data.data.message)
+        console.log('Merchant data = '+data.data.data)
+        if(data.data.data != ''){
+          console.log(111111)
+        }else
+        {
+          wx.showToast({
+            title: 'Server Error',//提示文字
+            duration:1000,//显示时长
+            mask:true,//是否显示透明蒙层，防止触摸穿透，默认：false  
+            icon:'loading', //图标，支持"success"、"loading"   
+         })
+        }
       }
     })
   },
 
   globalData: {
     userInfo: null,
+    openCode: null,
     host: 'https://www.snail2651.com',
     currentRestaurant: "Please choose the restaurant",
     userNumber: 1,
+    appID: 'wx9dc3a069c71ba1af',
+    key: '46c445a802736b5a068f29bc787d5160',
     QueueReadymoudle: "lKl7yagNE8783YzK7_NnAI6sSSK3ECgBQxCeolD9ISw",
     QueueCanclmoudle: "XV3X3kjwxtsZXce-0pa7zggvyKQMWjBs5iWHygSjPsQ",
     resInfo: [{
       "id": 0,
-      "name": "AAAA",
+      "name": "For Test1",
       "location": "ZhuHai-AAA-GGG-Road22",
       "phone": "12345678901",
       "status": "Closed",
@@ -69,7 +90,7 @@ App({
       "pic": "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fbjcache.leju.com%2Fzxjiaju%2Fzx_pic%2F20180509%2Fa0%2F67%2Fa670d844ebf06b368575a111aee6397c.jpeg&refer=http%3A%2F%2Fbjcache.leju.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1613294165&t=4bf09f335633fabe1ffee855883a4b7a"
     }, {
       "id": 1,
-      "name": "BBBBB",
+      "name": "For Test2",
       "location": "ZhuHai-AAA-GGG-Road22",
       "phone": "12345678901",
       "status": "Closed",
@@ -77,26 +98,26 @@ App({
       "pic": "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fbjcache.leju.com%2Fzxjiaju%2Fzx_pic%2F20161118%2Fde%2F96%2Fd96e483ca1f45a406ad2151c4ed46a63.jpeg&refer=http%3A%2F%2Fbjcache.leju.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1613294165&t=9ee35ba84b49efb6747b1e2d32e4f115"
     }, {
       "id": 2,
-      "name": "CCCCCCC",
+      "name": "For Test3",
       "location": "ZhuHai-AAA-GGG-Road22",
       "phone": "12345678901",
-      "status": "Opened",
+      "status": "Closed",
       "tag": ["Environment","Delicious"],
       "pic": "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpic1.shejiben.com%2Fcase%2F1208%2F11%2F20120811_26c7748df20b4169aaeaaoY939dnfjmX.jpg&refer=http%3A%2F%2Fpic1.shejiben.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1613294165&t=6282d21c65db23825cd23ffb9e9681b4"
     }, {
       "id": 3,
-      "name": "DDDDDDDD",
+      "name": "For Test4",
       "location": "ZhuHai-AAA-GGG-Road22",
       "phone": "12345678901",
-      "status": "Opened",
+      "status": "Closed",
       "tag": ["Environment","Price"],
       "pic": "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpic1.shejiben.com%2Fcase%2F2016%2F07%2F18%2F20160718104437-73843fc2.jpg&refer=http%3A%2F%2Fpic1.shejiben.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1613294165&t=10ea9c3a8accf5148c180cca3cec9ac5"
     }, {
       "id": 4,
-      "name": "EEEEEEEEEEE",
+      "name": "For Test5",
       "location": "ZhuHai-AAA-GGG-Road22",
       "phone": "12345678901",
-      "status": "Opened",
+      "status": "Closed",
       "tag": ["Environment","Delicious"],
       "pic": "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fm.tuniucdn.com%2Ffilebroker%2Fcdn%2Fsnc%2F57%2Ffc%2F57fc55155e4e8404f19b484088afd96f_w800_h400_c1_t0.jpg&refer=http%3A%2F%2Fm.tuniucdn.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1613294165&t=2c77d5c79e4f99c310ac9229bd3b88f5"
     }],
--- 小程序/pages/logs/logs.js
+++ 小程序/pages/logs/logs.js
@@ -11,37 +11,84 @@ Page({
   onLoad(options) {
     console.log(options)
     console.log(app.globalData.userInfo.nickName)
+    let self = this
     let userName = app.globalData.userInfo.nickName
     let userNum = app.globalData.userNumber
     let merchart = app.globalData.currentRestaurant
+    let tableType = 'small'
+    if(userNum>4 && userNum <= 6)
+    {
+      tableType = 'middle'
+    }else
+    {
+      tableType = 'big'
+    }
     wx.request({
-      url: app.globalData.host + '/line_up/',
-      method: 'POST',
-      data: null,
+      url: app.globalData.host + '/refresh',
+      method: 'GET',
+      data: {
+        merchant_name : merchart,
+        user_name : userName,
+      },
       success: function (data) {
-        console.log('line_up = '+data)
-        this.setData(
-          {
-            queueSuc:true,
-            queueMsg:10,
-            UserInfoForQueue: userName+' @ '+merchart+' USER NUMBER = '+userNum
-          }
-        )
-      },
-      fail: function(){
-          wx.showToast({
-            title: 'NetWork Error',//提示文字
-            duration:1000,//显示时长
-            mask:true,//是否显示透明蒙层，防止触摸穿透，默认：false  
-            icon:'loading', //图标，支持"success"、"loading"  
-            success:function(){ },//接口调用成功
-            fail: function () { },  //接口调用失败的回调函数  
-            complete: function () { } //接口调用结束的回调函数  
-         })
-      },
+        console.log('refresh = '+data.data.message)
+        if(data.data.code == -1) //只有没有排队的时候才进行请求
+        {
+          wx.request({
+            url: app.globalData.host + '/line_up',
+            method: 'POST',
+            data: {
+              merchant_name : merchart,
+              user_name : userName,
+              user_phone : null,
+              eat_number : userNum,
+              table_type : tableType,
+            },
+            success: function (data) {
+              console.log('line_up = '+data.data.code)
+              console.log('line_up = '+data.data.message)
+              if(data.data.code<0)
+              {
+                self.setData({
+                  queueSuc : false,
+                  queueMsg:'ERR',
+                  UserInfoForQueue: userName+' @ '+merchart+ 'err = ' +data.data.message,
+                })
+              }else
+              {
+                wx.setStorageSync("viewTitle", 'ERR')
+                wx.setStorageSync("viewInfo", userName+' @ '+merchart+' USER NUMBER = '+userNum)
+                wx.setStorageSync("viewStatus", false)
+                self.setData({
+                  queueSuc : false,
+                  queueMsg:'ERR',
+                  UserInfoForQueue: userName+' @ '+merchart+' USER NUMBER = '+userNum,
+                })
+              }
+            },
+            fail: function(){
+                wx.showToast({
+                  title: 'NetWork Error',//提示文字
+                  duration:1000,//显示时长
+                  mask:true,//是否显示透明蒙层，防止触摸穿透，默认：false  
+                  icon:'loading', //图标，支持"success"、"loading"  
+                  success:function(){ },//接口调用成功
+                  fail: function () { },  //接口调用失败的回调函数  
+                  complete: function () { } //接口调用结束的回调函数  
+               })
+            },
+          })
+        }else
+        {
+          console.log('no need live_up for '+data.data.message)
+        }
+      }
     })
+    
   },
+  onReady: function () {
 
+  },
   queuecancel: function(){
     wx.navigateBack();
   },
@@ -49,6 +96,14 @@ Page({
     wx.navigateTo({
       url: '../logs/logs'
     })
+  },
+  sleep:function(numberMillis) {
+    var now = new Date();
+    var exitTime = now.getTime() + numberMillis;
+    while(true) {
+      now = new Date();
+      if (now.getTime() > exitTime)
+        return;
+    }
   }
-
 })
